0c499dd7d8039a6d02f08179170a900179bd2880
EduardCon <EduardCon@github.com>
2018-04-12 16:59:40 +0200

    changes

--- src/networkHandler/NetworkHandlerSender.java
+++ src/networkHandler/NetworkHandlerSender.java
@@ -10,18 +10,39 @@ import java.net.InetAddress;
 import java.net.MulticastSocket;
 import java.net.UnknownHostException;
 
+
+/**
+ * Object that handles outgoing packets.
+ */
 public class NetworkHandlerSender {
 
+    /**
+     * The client that instantiated this object.
+     */
     private Client client;
+
+    /**
+     * The address of the multicast group.
+     */
     private InetAddress groupAddress;
-    private InetAddress myAddress;
+
+    /**
+     * The client's communication socket.
+     */
     private MulticastSocket socket;
 
+    /**
+     * Constructor.
+     * @param client The client that insantiates this object.
+     */
     public NetworkHandlerSender(Client client) {
         this.client = client;
         this.socket = this.client.getSocket();
     }
 
+    /**
+     * Method for connecting to the multicast group.
+     */
     public void connectToMultiCast() {
         try {
 
@@ -37,12 +58,23 @@ public class NetworkHandlerSender {
 
     }
 
+    /**
+     * Method for sending a message.
+     * @param message The message to be sent.
+     * @param port The destination port.
+     * @throws IOException
+     */
+
     public void send(String message, int port) throws IOException {
         DatagramPacket toSend = new DatagramPacket(message.getBytes(), message.length(), this.groupAddress, port);
         socket.send(toSend);
         System.out.println("Sent <" + message + "> to " + this.groupAddress + " on port: " + port);
     }
 
+    /**
+     * Getter.
+     * @return The client's communication socket.
+     */
     public MulticastSocket getSocket() {
         return this.socket;
     }
